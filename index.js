71ccf899ffc3e9cf54ca2f08288c76d2d5d5fa32
derek chen <derek.g.chen@gmail.com>
2018-03-03 23:42:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 function theBeatlesPlay(musicians, instruments){
   var temp=[]
+  var temp1=""
 
   for(let i =0; i < musicians.lengh; i++){
-
-    temp.push(musicians[i] + instruments[i])
+    temp1=musicians[i] + instruments[i]
+    temp.push( temp1 )
   }
   return temp
 
