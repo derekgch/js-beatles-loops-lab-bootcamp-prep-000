34008fbffecdd3eb48b7789615b986df9f501c06
derek chen <derek.g.chen@gmail.com>
2018-03-04 00:01:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,11 +22,11 @@ function johnLennonFacts(facts){
 
 function iLoveTheBeatles(n){
   var temp =[];
-  var i =1;
+  var i =0;
   do{
     temp.push("I love the Beatles!")
     i++
-  }while(n<15 && i<n)
+  }while(n<15 && i<n-1)
   return temp
 
 }
